e69377c3ef5b22f9ab9cc5a1c74adacabb946f68
Patrick José Pereira <patrickelectric@gmail.com>
2017-02-28 23:18:46 -0300

    Remove some unused stuff and add color change
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 #include <Qt3DRender/QGeometryRenderer>
 #include <Qt3DRender/QGeometry>
 
-#include <Qt3DExtras/QDiffuseMapMaterial>
+#include <Qt3DExtras/QPhongMaterial>
 #include <Qt3DExtras/QFirstPersonCameraController>
 #include <Qt3DExtras/QForwardRenderer>
 #include <Qt3DExtras/Qt3DWindow>
@@ -23,37 +23,15 @@ Qt3DRender::QGeometryRenderer *lineObject()
     auto mesh = new Qt3DRender::QGeometryRenderer();
     auto geometry = new Qt3DRender::QGeometry(mesh);
     auto dataBuffer = new Qt3DRender::QBuffer(Qt3DRender::QBuffer::VertexBuffer, geometry);
+    auto posAttribute = new Qt3DRender::QAttribute();
+
     QVector<QVector3D> vertices;
-
-    // Each line need two points.
-    // TODO: make lines with [n-1] [n] and not with [n*2] 
-
-    /*
-    vertices.resize(1e6);
+    vertices.resize(1e4);
     const int s = 20;
     for (auto& v : vertices) {
         v.setX(s/2 - rand() % s);
         v.setY(s/2 - rand() % s);
         v.setZ(s/2 - rand() % s);
-    }*/
-
-    for (uint i = 0; i < 2; i++) {
-        int x = qPow(-1, i);
-        for (uint j = 0; j < 2; j++) {
-            int y = qPow(-1, j);
-            for (uint k = 0; k < 2; k++) {
-                int z = qPow(-1, k);
-                vertices.prepend(QVector3D( 20*x, -20*y,  20*z));
-                vertices.prepend(QVector3D(-20*x, -20*y, -20*z));
-                vertices.prepend(QVector3D( 20*x,  20*y, -20*z));
-                vertices.prepend(QVector3D(-20*x,  20*y,  20*z));
-
-                vertices.append(QVector3D(-20*x, -20*y, -20*z));
-                vertices.append(QVector3D( 20*x, -20*y, -20*z));
-                vertices.append(QVector3D(-20*x, -20*y,  20*z));
-                vertices.append(QVector3D(-20*x,  20*y, -20*z));
-            }
-        }
     }
 
     QByteArray vertexBufferData;
@@ -68,14 +46,10 @@ Qt3DRender::QGeometryRenderer *lineObject()
 
     dataBuffer->setData(vertexBufferData);
 
-    auto posAttribute = new Qt3DRender::QAttribute();
     posAttribute->setAttributeType(Qt3DRender::QAttribute::VertexAttribute);
     posAttribute->setBuffer(dataBuffer);
     posAttribute->setDataType(Qt3DRender::QAttribute::Float);
     posAttribute->setDataSize(3);
-    posAttribute->setByteOffset(0);
-    posAttribute->setByteStride(0);
-    posAttribute->setCount(vertices.size());
     posAttribute->setName(Qt3DRender::QAttribute::defaultPositionAttributeName());
 
     geometry->addAttribute(posAttribute);
@@ -107,17 +81,17 @@ int main(int argc, char* argv[])
     auto camController = new Qt3DExtras::QFirstPersonCameraController(root);
     camController->setCamera(camera);
 
-    auto cube = new Qt3DCore::QEntity();
-    auto cubeTransform = new Qt3DCore::QTransform();
+    auto lines = new Qt3DCore::QEntity();
+    auto linesTransform = new Qt3DCore::QTransform();
 
-    Qt3DRender::QGeometryRenderer *indicatorMesh = lineObject();
-    Qt3DRender::QMaterial *material = new Qt3DExtras::QDiffuseMapMaterial(root);
+    Qt3DRender::QGeometryRenderer *linesMesh = lineObject();
+    auto material = new Qt3DExtras::QPhongMaterial(root);
+    material->setAmbient(QColor(0, 120, 0));
 
-    cube->addComponent(indicatorMesh);
-    cube->addComponent(material);
-    cube->addComponent(cubeTransform);
-    cube->setParent(root);
-    cube->setEnabled(true);
+    lines->addComponent(linesMesh);
+    lines->addComponent(material);
+    lines->addComponent(linesTransform);
+    lines->setParent(root);
 
     view.setRootEntity(root);
     view.show();
